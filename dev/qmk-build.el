c4f52d4f2fb84a65be6ba1d4779f21254a16d387
duncan britt <dbru997@gmail.com>
2026-07-05 00:42:34 -0600

    tap hold for capslock and semicolon
    Capslock is left control on hold and escape on tap.
    Semicolon is left alt on hold and semicolon on tap.

---
+++ dev/qmk-build.el
@@ -0,0 +1,20 @@
+;;; qmk-build.el --- Build and flash commands for qmk configurator -*- lexical-binding: t -*-
+
+(defun qmk-build ()
+  "Compile firmware."
+  (interactive)
+  (let ((default-directory (project-root (project-current))))
+    (compile "qmk compile -kb keychron/v1/ansi -km Duncan_Britt" t)))
+;; qmk config user.keyboard=keychron/v1/ansi
+;; qmk config user.keymap=Duncan_Britt
+
+;; Tap hold: KC_LEFT_CTRL -> MOD_LCTL
+;; MT(mod, kc)
+
+;; Left control on hold, escape on tap:
+;; MT(MOD_LCTL, KC_ESC) or LCTL_T(KC_ESC) or CTL_T(KC_ESC)
+
+;; Left alt on hold, semicolon on tap:
+;; MT(MOD_LALT, KC_SCLN)
+
+(provide 'qmk-build)
